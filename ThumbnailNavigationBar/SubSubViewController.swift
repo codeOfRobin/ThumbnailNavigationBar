9252715056336d35eb132609a82ae6e431cf3cf1
Robin Malhotra <me@rmalhotra.com>
2018-10-07 21:43:52 +0530

    Completed VCs
    Signed-off-by: Robin Malhotra <me@rmalhotra.com>

--- ThumbnailNavigationBar/SubSubViewController.swift
+++ ThumbnailNavigationBar/SubSubViewController.swift
@@ -10,8 +10,23 @@ import UIKit
 
 class SubSubViewController: UITableViewController {
 
+    var headerView: ThumbnailHeaderImageView?
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.title = "Firewatch"
+        self.navigationItem.largeTitleDisplayMode = .never
+        self.tableView.contentInsetAdjustmentBehavior = .never
+
+        self.headerView = ThumbnailHeaderImageView(image: UIImage(named: "Firewatch")!, height: 200)
+        self.headerView?.shadowHidden = false
+        self.tableView.tableHeaderView = self.headerView
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.thumbnailNavigationBar?.setBackgroundHidden(true, animated: animated, for: self)
+        self.navigationController?.thumbnailNavigationBar?.setTargetScrollView(self.tableView, minimumOffset: 200)
     }
 
     // MARK: - Table view data source
@@ -25,56 +40,18 @@ class SubSubViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        var cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        if cell == nil {
+            cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+            cell?.accessoryType = .disclosureIndicator
+        }
 
-         Configure the cell...
-
-        return cell
+        cell?.textLabel?.text = "Tap here for normal bar"
+        return cell!
     }
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    override func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        self.headerView?.scrollOffset = scrollView.contentOffset.y
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
